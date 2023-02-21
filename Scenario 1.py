c660af1e668a178e1bafbf9f2e21e97e1ea56812
YoWhat45 <yowhat58@gmail.com>
2023-02-21 16:50:30 +0800

    SUP

--- Scenario 1.py
+++ Scenario 1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plot
 
 travel_df = pd.read_excel('./Project_File.xlsx', index_col=0)
 travel_df.columns = travel_df.columns.str.strip()
@@ -16,7 +17,12 @@ other_region_df = travel_df[['USA', 'Canada', 'Australia', 'New Zealand', 'Afric
 print(other_region_df)
 
 #Choosing Years
+years_other_region_df = other_region_df["2008 Jan": "2017 Nov"]
 
-years_other_region_df = other_region_df
+print(years_other_region_df)
+
+#Plotting
+
+
 
 
