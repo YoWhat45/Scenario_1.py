840c11ac2927dc6536eb738585632bed876b4451
YoWhat45 <yowhat58@gmail.com>
2023-02-21 16:10:04 +0800

    SUP

--- Scenario 1.py
+++ Scenario 1.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import numpy as np
 
-travel_df = pd.read_excel('./Project_File.xlsx')
+travel_df = pd.read_excel('./Project_File.xlsx', index_col=0)
+travel_df.columns = travel_df.columns.str.strip()
+travel_df.index = travel_df.index.str.strip()
 travel_df.head()
+travel_df.tail()
+travel_df.shape
 
 print(travel_df)
 
-other_region_travel_df =
+#Choosing other region
+other_region_df = travel_df[['USA', 'Canada', 'Australia', 'New Zealand', 'Africa']]
+
+print(other_region_df)
+
+#Choosing Years
+
+years_other_region_df = other_region_df
+
 
