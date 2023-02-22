e006043276f1d06c316c9869c5aace571539c857
YoWhat45 <yowhat58@gmail.com>
2023-02-22 21:17:33 +0800

    SUP

--- Scenario 1.py
+++ Scenario 1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import unittest
 
 travel_df = pd.read_excel('./Project_File.xlsx', index_col=0)
 travel_df.columns = travel_df.columns.str.strip()
@@ -29,23 +30,40 @@ Australia = plot_graph['Australia']
 NewZealand = plot_graph['New Zealand']
 Africa = plot_graph['Africa']
 
-plt.plot(USA)
-plt.plot(Canada)
-plt.plot(Australia)
-plt.plot(NewZealand)
-plt.plot(Africa)
+plt.plot(USA, label= 'USA')
+plt.plot(Canada, label = 'Canada')
+plt.plot(Australia, label = 'Australia')
+plt.plot(NewZealand, label = 'New Zealand')
+plt.plot(Africa, label = 'Africa')
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 plt.title('Other regions in years 2008 - 2017')
 plt.xlabel('Month')
 plt.ylabel('Country')
-plt.legend(fontsize = 10)
+plt.legend()
 plt.xticks(rotation = 90)
 plt.margins(x=0, y=0)
 
 plt.show()
+
+#Top 3 countries in the region over a span of 10 years.
+top_3_countries_df = years_other_region_df
+
+
 #Test Class
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 
