fbf6cc803211b53dda472be3f18fd364580f168a
YoWhat45 <yowhat58@gmail.com>
2023-02-22 10:18:37 +0800

    SUP

--- Scenario 1.py
+++ Scenario 1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plot
+import matplotlib.pyplot as plt
 
 travel_df = pd.read_excel('./Project_File.xlsx', index_col=0)
 travel_df.columns = travel_df.columns.str.strip()
@@ -22,6 +22,23 @@ years_other_region_df = other_region_df["2008 Jan": "2017 Nov"]
 print(years_other_region_df)
 
 #Plotting
+plot_graph = years_other_region_df
+
+USA = plot_graph['USA']
+Canada = plot_graph['Canada']
+Australia = plot_graph['Australia']
+NewZealand = plot_graph['New Zealand']
+Africa = plot_graph['Africa']
+
+plt.figure(facecolor = 'white', figsize = (8, 4))
+plt.plot(USA)
+plt.plot(Canada)
+plt.plot(Australia)
+plt.plot(NewZealand)
+plt.plot(Africa)
+
+plt.show()
+#Test Class
 
 
 
