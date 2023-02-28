cf9669ff19f78aa4648e3c97eb3c452f5c797828
YoWhat45 <yowhat58@gmail.com>
2023-02-28 13:36:45 +0800

    SUP

--- DA_Group10.py
+++ DA_Group10.py
@@ -2,23 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 travel_df = pd.read_excel('./Project_File.xlsx')
-travel_df.head()
-travel_df.tail()
-travel_df.shape
+
+#View the new dataframe
+print(travel_df.head())
+print(travel_df.tail())
+print(travel_df.shape)
 
 #Rename first column
 travel_df.rename(columns={travel_df.columns[0]: "Year" }, inplace = True)
 
 print(travel_df)
 
+#Remove white space
+travel_df.columns = travel_df.columns.str.strip()
+#travel_df.index = travel_df.index.str.strip()
+
 #Converting
 travel_df = travel_df.astype('str')
 
 print(travel_df.dtypes)
-
-#Remove white space
-travel_df.columns = travel_df.columns.str.strip()
-#travel_df.index = travel_df.index.str.strip()
 
 #Split Year and Month
 travel_df[['Year', 'Month']] = travel_df['Year'].str.split(n=1, expand=True)
@@ -44,9 +46,9 @@ sum_travel_df = sum_travel_df.sum()
 print(sum_travel_df)
 
 #Plot Graph
-plot_graph = sum_travel_df.sort_values()
+plot_graph = sum_travel_df.sort_values(ascending=False)
 
-ax = plot_graph.plot(kind = 'bar', title = 'Amount of Visitors from 2008 - 2017', color = 'black')
+ax = plot_graph.plot(kind = 'bar', title = 'Amount of Visitors from 2008 - 2017', color = 'magenta')
 plt.ticklabel_format(style='plain', axis='y')
 plt.xticks(rotation = 0)
 plt.xlabel("Country Name", size = 14)
